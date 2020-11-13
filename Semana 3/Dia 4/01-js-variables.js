c13429fcac6f3872cfc4ab3cb4acc05e7e6b15ba
Giancarlo-Tapia <73673702+Giancarlo-Tapia@users.noreply.github.com>
2020-11-12 22:14:29 -0500

    Commit  Github + Java

---
+++ Semana 3/Dia 4/01-js-variables.js
@@ -0,0 +1,24 @@
+// Variables en javascript
+
+// Variable tipo number - 
+let edad= 18;
+let sueldo= 950.50;
+
+//Variables de tipo de cadena de texto
+let nombre = "Giancarlo Tapia Torrejón"
+let mensaje = "Se que estás ahí"
+
+//Variables tipo Boolean
+let casado = false;
+let cazado = true;
+
+
+//Variables de tipo arreglo a array
+let platos = ["Ceviche" , "Seco" , "Causa"];
+
+// Console.log()
+
+// Es para imprimir en la consola del navegador
+console.log("algo como esto");
+console.log(cazado);
+console.log("casado" + casado);
