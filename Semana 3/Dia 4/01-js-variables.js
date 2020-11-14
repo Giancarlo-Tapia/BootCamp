b63879125ff3c2e464cb71b4d3b48d388388e01b
Giancarlo-Tapia <73673702+Giancarlo-Tapia@users.noreply.github.com>
2020-11-14 11:16:58 -0500

    JS. Variables Cond. + Secu.

--- Semana 3/Dia 4/01-js-variables.js
+++ Semana 3/Dia 4/01-js-variables.js
@@ -22,3 +22,20 @@ let platos = ["Ceviche" , "Seco" , "Causa"];
 console.log("algo como esto");
 console.log(cazado);
 console.log("casado" + casado);
+console.log(`Otra nueva línea`);
+
+// plantillas o templates usando `BACKTICKS`
+console.log(`Mi nombre es: ${nombre} y tengo ${edad} años`);
+
+// Modificar un valor de una variable ya creada
+edad = 32;
+console.log(`Corrección!, mi edad es de ${edad} años`);
+
+console.log(`Mis platos favoritos son ${platos}`);
+
+let mensajeCompleto = `Mi nombre es ${nombre} y tengo ${edad} años`;
+
+// Constante, se crean con el identificador "const"
+
+const pi = 3.1416;
+console.log(`Valor de PI = ${pi}`);
