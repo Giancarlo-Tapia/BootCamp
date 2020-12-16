7d5873b7faa488a723af9d6309a9e6527bd9fa6d
Giancarlo-Tapia <73673702+Giancarlo-Tapia@users.noreply.github.com>
2020-12-15 22:41:19 -0500

    useEfect - Ejercicio con usuarios

---
+++ Semana 8/Dia 2/react-peliculas/components/CardPeliculas.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { peliculas } from "./Data/data";
+
+const CardPeliculas = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default CardPeliculas
